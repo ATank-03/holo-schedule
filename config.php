53fb473119fe638d135f0b5d68dd56d58c9f8aed
Atilla Tank <atillatank@gmail.com>
2026-02-12 12:29:01 +0100

    currently all I've made with cursor

--- config.php
+++ config.php
@@ -8,6 +8,6 @@ return [
     // YouTube Data API v3 key, gebruikt voor het automatisch importeren
     // van geplande livestreams in de app via de Google API.
     // Vul hier je API key in, of zet hem als environment variable YOUTUBE_API_KEY.
-    'youtube_api_key' => getenv('YOUTUBE_API_KEY') ?: 'YOUR_YOUTUBE_API_KEY_HERE',
+    'youtube_api_key' => getenv('YOUTUBE_API_KEY') ?: 'AIzaSyAQeLPlEnOyJ499_tjqG-I0PkrJAWrLofY',
 ];
 
